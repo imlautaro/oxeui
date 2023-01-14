a3d042a23d3562773c5b110aa5fb709dad9bcb36
Lautaro Pereyra <hi@imlautaro.com>
2023-01-14 17:43:06 -0300

    fix: set line-height in counter badge

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -7,7 +7,7 @@ defineProps<{ value?: number }>()
 		<span
 			v-if="value"
 			class="bg-primary-600 absolute flex top-0 right-0 -translate-y-1/3 rounded-full px-1.25 font-bold text-white"
-			style="font-size: 0.625rem"
+			style="font-size: 0.625rem; line-height: 1rem"
 		>
 			{{ value > 99 ? '+99' : value }}
 		</span>
