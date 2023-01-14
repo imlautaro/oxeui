bd487fdfa1c51d5fddb0d19d991702c1e65f08df
Lautaro Pereyra <hi@imlautaro.com>
2023-01-14 10:18:46 -0300

    add counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{ value?: number }>()
+</script>
+
+<template>
+	<div class="relative">
+		<span
+			v-if="value"
+			class="bg-primary-600 absolute flex top-0 right-0 -translate-y-1/3 rounded-full px-1.25 font-bold text-white"
+			style="font-size: 0.625rem"
+		>
+			{{ value > 99 ? '+99' : value }}
+		</span>
+		<slot />
+	</div>
+</template>
